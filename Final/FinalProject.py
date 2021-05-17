aa9c9b102f98f2380f7ee67a357130ecbdc6e570
AidanMPeterson <83615362+AidanMPeterson@users.noreply.github.com>
2021-05-16 19:40:13 -0700

    Positions are numbered top-to-bottom

--- Final/FinalProject.py
+++ Final/FinalProject.py
@@ -65,7 +65,7 @@ class TicTacToe:
 
         "board" is a list of 10 strings representing the board (ignore index 0)'''
         print('   |   |')
-        print(' ' + self.board[7] + ' | ' + self.board[8] + ' | ' + self.board[9])
+        print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
         print('   |   |')
         print('-----------')
         print('   |   |')
@@ -73,7 +73,7 @@ class TicTacToe:
         print('   |   |')
         print('-----------')
         print('   |   |')
-        print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
+        print(' ' + self.board[7] + ' | ' + self.board[8] + ' | ' + self.board[9])
         print('   |   |')
 
     def getPlayerMove(self, letter):
